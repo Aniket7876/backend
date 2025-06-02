feddf94e4e36d05f35a143e83c16f6e66fdd6e7b
Aniket7876 <aniketgore700@gmail.com>
2025-06-02 18:21:28 +0530

    test

--- config/swagger/swagger.js
+++ config/swagger/swagger.js
@@ -16,7 +16,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: `https://13-232-24-179:5000`,
+        url: `http://3.110.136.41:5000`,
         description: 'Development server',
       },
     ],
--- server.js
+++ server.js
@@ -4,14 +4,6 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const morgan = require('morgan');
 const swaggerDocs = require('./config/swagger/swagger');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-
-const sslOptions = {
-  key: fs.readFileSync(path.join(__dirname, 'ssl', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'ssl', 'cert.pem'))
-};
 
 dotenv.config();
 
@@ -39,6 +31,6 @@ swaggerDocs(app);
 
 const PORT = process.env.PORT || 5000;
 
-https.createServer(sslOptions, app).listen(PORT, () => {
-  console.log(`HTTPS Server running at https://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
